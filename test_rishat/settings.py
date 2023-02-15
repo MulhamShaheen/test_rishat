e129cd0dc51df82ceb67303a33a5ea5560e93824
MulhamShaheen <mulhamshaheen.ms@gmail.com>
2023-02-15 11:11:38 +0500

    localhost stuff

--- test_rishat/settings.py
+++ test_rishat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-94@s03k=b3siq1pg0ptopoky_*3p#txcr+_^6f*(+zok_^*cxh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
